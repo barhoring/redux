0ad4a3a4cf10a33c382a93f9b6b1f154576e6bdc
Bar Horing Amir <bar.apple@gmail.com>
2019-07-29 17:25:52 +0300

    create a store

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 import React from "react";
 import { render } from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import PlayerReducer from "./src/reducers/player";
 import Scoreboard from "./src/containers/Scoreboard";
 
-render(<Scoreboard />, document.getElementById("root"));
+const store = createStore(PlayerReducer);
+
+render(
+  <Provider store={store}>
+    <Scoreboard />
+  </Provider>,
+  document.getElementById("root")
+);
